72a5f8c5a24446b5e7175259626327fb81c75eab
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-24 13:33:36 -0600

    create fetch func and test it

---
+++ src/utilities/fetchCall.js
@@ -0,0 +1,7 @@
+const fetchCall = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+export default fetchCall;
---
+++ src/utilities/tests/fetchCall.test.js
@@ -0,0 +1,18 @@
+import fetchCall from '../fetchCall';
+
+describe('fetchCall', () => {
+  let mockUrl;
+
+  beforeEach(() => {
+    mockUrl = ''
+  })
+
+  it('should call fetch w/ the correct params', async () => {
+    window.fetch = jest.fn().mockImplementation(() => (
+      Promise.resolve({ json: () => Promise.resolve('Promise resolved') })
+    ))
+
+    await fetchCall(mockUrl);
+    expect (window.fetch).toHaveBeenCalledWith(mockUrl);
+  })
+})
