ba69b00c51203015f22b9ac3e2b424b86189784a
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-24 15:48:12 -0600

    implement redux on app

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import React, { Component } from 'react';
 
 import fetchCall from './utilities/fetchCall';
 import { apiKey } from './utilities/apiKey';
-import * as Cleaners from './utilities/cleaners';
+// import * as Cleaners from './utilities/cleaners';
 import SingleMovie from './components/SingleMovie/'
+import { connect } from 'react-redux';
+
+import { getMovieList } from './actions';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      fetchCall: fetchCall,
-      movies: []
-    }
-  }
 
   async componentDidMount() {
-    const movies = await fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=db393102c6d38bb71fed129f3aaf36d8&/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2018-10-23`)
-    this.setState({ movies: movies.results });
+    const filmObject = await fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2018-10-23`)
+    this.props.setFetchedMovies(filmObject.results)
   }
-  
-
 
   posters = () => {
-    if (this.state.movies.length) {
-      return this.state.movies.map( movie => (
-        <SingleMovie {...movie} />
+    if (this.props.movies.length > 1) {
+      return this.props.movies.map( movie => (
+        <SingleMovie key={movie.title} {...movie} />
         ))
     } else {
-      return <SingleMovie />
+      return ''
     }
   }
   
@@ -35,9 +29,18 @@ class App extends Component {
     return (
       <div className="App">
         { this.posters() }
+        <p>test</p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  movies: state.movies
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setFetchedMovies: (data) => dispatch(getMovieList(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
