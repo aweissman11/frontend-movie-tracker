d394dbb9f397c6f7f602177e51caeba9dd5246a3
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-24 14:35:34 -0600

    fetch data, map over results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import fetchCall from './utilities/fetchCall';
+import { apiKey } from './utilities/apiKey';
+import * as Cleaners from './utilities/cleaners';
+import SingleMovie from './components/SingleMovie/'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fetchCall: fetchCall,
+      movies: []
+    }
+  }
+
+  async componentDidMount() {
+    const movies = await fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=db393102c6d38bb71fed129f3aaf36d8&/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2018-10-23`)
+    this.setState({ movies: movies.results });
+  }
+  
+
+
+  posters = () => {
+    if (this.state.movies.length) {
+      return this.state.movies.map( movie => (
+        <SingleMovie {...movie} />
+        ))
+    } else {
+      return <SingleMovie />
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { this.posters() }
       </div>
     );
   }
