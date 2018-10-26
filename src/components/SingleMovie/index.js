62f9997133f898ccd1365036ccff33ab0327caa6
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 13:54:47 -0600

    replace html with favoriteBtn jsx

--- src/components/SingleMovie/index.js
+++ src/components/SingleMovie/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import FavoriteBtn from '../../containers/FavoriteBtn';
+
 const SingleMovie = (props) => (
   <div>
     <h2>{props.title}</h2>
-    <button className='favorite-btn'>&hearts;</button>
+    <FavoriteBtn />
     <img src={`https://image.tmdb.org/t/p/w400_and_h600_bestv2${props.poster_path}`} alt={props.title}/>
   </div>
 )
