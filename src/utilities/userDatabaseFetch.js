33b447b0d4afcdc24f31339514e5072e853f252a
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 16:27:57 -0600

    add addfavorite and removefavorite to userdatabasefetch

--- src/utilities/userDatabaseFetch.js
+++ src/utilities/userDatabaseFetch.js
@@ -29,3 +29,39 @@ export const createNewUser = async (data) => {
   const response = await fetch(url, optionsObject);
   return await response.json();
 }
+
+export const addFavorite = async (data) => {
+  const url = 'http://localhost:3000/api/users/favorites/new'
+
+  const optionsObject = {
+    method: "POST", 
+    body: JSON.stringify(data),
+    credentials: "same-origin", 
+    headers: {
+        "Content-Type": "application/json",
+    }
+  }
+
+  const response = await fetch(url, optionsObject);
+  return await response.json();
+}
+
+export const removeFavorite = async (userId, movieId) => {
+  const url = `http://localhost:3000/api/users/${userId}/favorites/${movieId}`
+
+  const optionsObject = {
+    method: "DELETE", 
+    body: JSON.stringify({
+      user_id: userId,
+      movie_id: movieId
+    }),
+    credentials: "same-origin", 
+    headers: {
+        "Content-Type": "application/json",
+    }
+  }
+
+  const response = await fetch(url, optionsObject);
+  return await response.json();
+}
+
