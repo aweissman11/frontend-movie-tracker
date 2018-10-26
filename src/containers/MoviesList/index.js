8446e81bd480040e518a748f2d0a307dd67dafdd
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 16:39:17 -0600

    begin to move updateFavorites to moviesList

--- src/containers/MoviesList/index.js
+++ src/containers/MoviesList/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import fetchCall from '../../utilities/fetchCall';
-import { getMovieList } from '../../actions';
+import { getMovieList, updateFavorites } from '../../actions';
 import { apiKey } from '../../utilities/apiKey';
 
 import SingleMovie from '../../components/SingleMovie';
@@ -14,6 +14,25 @@ class MoviesList extends Component {
   async componentDidMount() {
     const filmObject = await fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&/movie?primary_release_date.lte=2018-10-23`)
     this.props.setFetchedMovies(filmObject.results)
+    if (this.props.user.id) {
+      const favorites = await this.getFavorites();
+      console.log('favorites:', favorites);
+      this.props.setFavorites(favorites.data)
+    }
+  }
+
+  async componentDidUpdate() {
+    const filmObject = await fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&/movie?primary_release_date.lte=2018-10-23`)
+    this.props.setFetchedMovies(filmObject.results)
+    if (this.props.user.id) {
+      const favorites = await this.getFavorites();
+      this.props.setFavorites(favorites.data)
+    }
+  }
+
+  getFavorites = async () => {
+    const url = `http://localhost:3000/api/users/${this.props.user.id}/favorites`
+    return await fetchCall(url)    
   }
 
   getMovies = () => {
@@ -40,11 +59,13 @@ class MoviesList extends Component {
 
 const mapStateToProps = (state) => ({
   movies: state.movies,
-  user: state.user
+  user: state.user,
+  favorites: state.favorites
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  setFetchedMovies: (data) => dispatch(getMovieList(data))
+  setFetchedMovies: (data) => dispatch(getMovieList(data)),
+  setFavorites: (data) => dispatch(updateFavorites(data))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(MoviesList);
