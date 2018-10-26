218a5c40eb087ec0d7c5ee65bead41c9cef8f6be
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 16:27:05 -0600

    build out favoriteBtn

--- src/containers/FavoriteBtn/index.js
+++ src/containers/FavoriteBtn/index.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export class FavoriteBtn extends Component {
+import * as userDataBaseFetch from '../../utilities/userDatabaseFetch';
+import fetchCall from '../../utilities/fetchCall';
+import { updateFavorites } from '../../actions';
 
-  toggleFavorite = (movieId) => {
-    
+export class FavoriteBtn extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isFavorite: false
+    }
+  }
+
+  async componentDidMount() {
+    if (this.props.user.id) {
+      const favorites = await this.getFavorites();
+      this.props.setFavorites(favorites)
+      if (favorites.data.find(favorite => favorite.movie_id === this.props.movieId)) {
+        this.setState({ isFavorite: true })
+      }
+    }
+  }
+
+  toggleFavorite = async (movieId) => {
+    const { movies, user } = this.props;
+    if (!user.id) {
+      console.walogrn("You're not logged in!");
+    } else {
+      const favorites = await this.getFavorites();
+      if (!favorites.data.find(favorite => favorite.movie_id === movieId)) {
+        try {
+          await userDataBaseFetch.addFavorite(this.formatFavorite(movies, user, movieId));
+          this.setState({ isFavorite: true })
+          console.log('add favorite')
+        } catch(error) {
+          console.error(error);
+        }
+      } else {
+        await this.removeFavorite(user.id, movieId);
+        this.setState({ isFavorite: false })
+        console.log('remove favorite')
+      }
+    }
+  }
+
+  removeFavorite = async (userId, movieId) => {
+    await userDataBaseFetch.removeFavorite(userId, movieId)
+  }
+  
+  formatFavorite = (movies, user, movieId) => {
+    const movie = movies.filter( movie => movie.id === movieId)[0]
+    return {
+      movie_id: movie.id,
+      user_id: user.id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      release_date: movie.release_date,
+      vote_average: movie.vote_average,
+      overview: movie.overview,
+      genre_ids: movie.genre_ids
+    }
+  }
+
+  getFavorites = async () => {
+    const url = `http://localhost:3000/api/users/${this.props.user.id}/favorites`
+    return await fetchCall(url)    
   }
 
   render() {
     return (
     <button
-      onClick={() => this.props.toggleFavorite(this.props.movieId)}
+      onClick={() => this.toggleFavorite(this.props.movieId)}
       className='favorite-btn'
     >&hearts;</button>
     )
   }
 }
 
-export default FavoriteBtn;
+const mapStateToProps = ({movies, user, favorites}) => ({movies, user, favorites});
+
+const mapDispatchToProps = (dispatch) => ({
+  setFavorites: (data) => dispatch(updateFavorites(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(FavoriteBtn);
