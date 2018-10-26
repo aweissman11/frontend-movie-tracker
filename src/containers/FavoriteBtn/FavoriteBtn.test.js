afe778bdb2dab13f25c0154a87f21e2ca8cb1b91
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 13:55:04 -0600

    create FavoriteBtn test

---
+++ src/containers/FavoriteBtn/FavoriteBtn.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import FavoriteBtn from './index';
+
+describe('FavoriteBtn', () => {
+  it('should create an action on click', () => {
+    //
+  })
+
+  it('should check if the user is logged in', () => {
+
+  })
+
+  it('should post a fetch with the correct parameters', () => {
+
+  })
+
+  it('should be isFavorited if it has been favorited', () => {
+    
+  })
+})
