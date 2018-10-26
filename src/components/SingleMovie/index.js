274922217373fe3eb36fd3c151c5527c48050dbe
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 11:38:09 -0600

    add favorite btn

--- src/components/SingleMovie/index.js
+++ src/components/SingleMovie/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SingleMovie = (props) => (
   <div>
     <h2>{props.title}</h2>
+    <button className='favorite-btn'>&hearts;</button>
     <img src={`https://image.tmdb.org/t/p/w400_and_h600_bestv2${props.poster_path}`} alt={props.title}/>
   </div>
 )
