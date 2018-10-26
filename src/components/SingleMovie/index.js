0fc9fc2b9fb99f1dc296b7e9af179048f5c0bab9
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 16:26:47 -0600

    pass movieId down through favoriteBtn for easy matching

--- src/components/SingleMovie/index.js
+++ src/components/SingleMovie/index.js
@@ -5,7 +5,7 @@ import FavoriteBtn from '../../containers/FavoriteBtn';
 const SingleMovie = (props) => (
   <div>
     <h2>{props.title}</h2>
-    <FavoriteBtn />
+    <FavoriteBtn movieId={props.id}/>
     <img src={`https://image.tmdb.org/t/p/w400_and_h600_bestv2${props.poster_path}`} alt={props.title}/>
   </div>
 )
