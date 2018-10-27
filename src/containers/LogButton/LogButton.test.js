920483b16bab7765002a068fcc6fddfb27367445
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 18:48:26 -0600

    test mstp and mdtp

--- src/containers/LogButton/LogButton.test.js
+++ src/containers/LogButton/LogButton.test.js
@@ -22,4 +22,34 @@ describe('LogButton', () => {
     wrapper = shallow(<LogButton user={{id: 2}}/>)
     expect(wrapper).toMatchSnapshot();
   })
+  
+  describe('mapStateToProps', () => {
+    it('should map the state to props', () => {
+      const mockUser = {
+          name: 'Aaron',
+          id: 1
+      }
+
+      const expected = mockUser
+      const mockState = {
+        user: {...mockUser},
+      }
+      
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps.user).toEqual(expected)
+    })
+    
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should map the dispatch to props', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = Actions.logUserOut()
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      
+      mappedProps.logOut()
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+  })
 })
