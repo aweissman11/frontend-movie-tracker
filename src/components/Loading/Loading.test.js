feb7b5d1d4640f68f5e26d7181ec19ea39c014a3
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-26 18:31:01 -0600

    Loading snapshot test

---
+++ src/components/Loading/Loading.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Loading from './index';
+import { shallow } from 'enzyme';
+
+describe('Loading', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<Loading />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
