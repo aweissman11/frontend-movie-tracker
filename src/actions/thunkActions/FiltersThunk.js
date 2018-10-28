c8be5ef9169456fc1a4d84d6a0848e408e086e70
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-28 10:12:48 -0600

    add FiltersThunk

---
+++ src/actions/thunkActions/FiltersThunk.js
@@ -0,0 +1,18 @@
+import * as Cleaners from '../../utilities/cleaners';
+import fetchCall from '../../utilities/fetchCall';
+
+export const getMovieList = (query) => {
+  return async (dispatch) => {
+    try {
+      console.log('query:', query);
+      const movies = await fetchCall(Cleaners.getFilterUrl(query));
+      dispatch({
+        type: 'GET_MOVIE_LIST',
+        movies: movies
+      })
+    } catch(e) {
+      dispatch({ type: 'ERROR'})
+    }
+  }
+}
+
