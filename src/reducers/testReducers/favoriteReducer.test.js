6d787438ae63d8fdfe6e97618058e34e785a3777
Laura Shamus <lkshamus@gmail.com>
2018-10-28 14:00:34 -0600

    Favoties reducer test

---
+++ src/reducers/testReducers/favoriteReducer.test.js
@@ -0,0 +1,31 @@
+import favoritesReducer from '../favoritesReducer';
+import * as Actions from '../../actions';
+
+describe('favoritesReducer', () => {
+  it('should return the default state', () => {
+    const expected = []
+
+    const result = favoritesReducer(undefined, {})
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should return state with favorites', () => {
+    const expected = [
+      {
+        title: 'Mean Girls'
+      },
+      {
+        title: 'Back to the future'
+      }
+    ]
+    const mockAction = {
+      type: 'UPDATE_FAVORITES',
+      favorites: expected
+    }
+
+    const result = favoritesReducer(null, mockAction)
+
+    expect(result).toEqual(expected)
+  })  
+})
