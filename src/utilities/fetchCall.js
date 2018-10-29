6acf35d73f0fe5c72fb28e25b1348b62f0fa9ae6
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-29 14:05:11 -0600

    add response check to fetchCall

--- src/utilities/fetchCall.js
+++ src/utilities/fetchCall.js
@@ -1,5 +1,9 @@
 const fetchCall = async (url) => {
   const response = await fetch(url);
+  if (!response.ok) {
+    console.error(Error(response.statusText));
+    return 'failed'
+  }
   return await response.json();
 }
 
