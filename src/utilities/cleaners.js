1d07a46483d15eb1acbf65b676a1461cf53d1ecb
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-29 12:40:28 -0600

    fix cleaner to create just one url for search or filter

--- src/utilities/cleaners.js
+++ src/utilities/cleaners.js
@@ -24,36 +24,7 @@ export const getMoviePosterUrl = (movies) => {
 
 export const today = getTodaysDate()
 
-export const movieList = fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&/movie?primary_release_date.lte=${today}`)
-
-export const getFilterUrl = (filterProperties) => {
-  const today = getTodaysDate();
-
-  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US?primary_release_date.lte=${today}&sort_by=popularity.desc`
-
-  const { genre, year, rating, sort } = filterProperties;
-  if (genre && genre !== 'GENRE') {
-    url += `&with_genres=${genre}`
-  }
-  
-  if (year && year !== 'YEAR') {
-    url += `&primary_release_year=${year}`
-  }
-
-  if (rating && rating !== 'RATING') {
-    url += `&certification_country=US&certification=${rating}`
-  }
-
-  if (sort && sort !== 'SORT') {
-    url += `&sort_by=${sort}`
-  }
-
-  return url;
-}
-
-export const getSearchUrl = (query) => {
-  return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
-}
+export const movieList = fetchCall(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&/movie?primary_release_date.lte=${today}&append_to_response=videos`)
 
 export const getFavoritesInfo = async (IDs) => {
   return Promise.all(IDs.map( async (id) => {
@@ -65,4 +36,37 @@ export const getFavoritesInfo = async (IDs) => {
 export const getCurrentFavorites = async (id) => { 
   const url = `http://localhost:3000/api/users/${id}/favorites`;
   return await fetchCall(url);
+}
+
+export const getFullUrl = (filterProperties, searchQuery) => {
+  let url;
+  const today = getTodaysDate();
+
+  if (searchQuery.length < 1) {
+    console.log('if')
+    url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US?primary_release_date.lte=${today}&sort_by=popularity.desc`
+  } else {
+    console.log('else')
+    url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchQuery}&page=1&include_adult=false`
+  }
+  
+  const { genre, year, rating, sort } = filterProperties;
+  if (genre && genre !== 'GENRE') {
+    url += `&with_genres=${genre}`
+  }
+  
+  if (year && year !== 'YEAR') {
+    url += `&primary_release_year=${year}`
+  }
+  
+  if (rating && rating !== 'RATING') {
+    url += `&certification_country=US&certification=${rating}`
+  }
+  
+  if (sort && sort !== 'SORT') {
+    url += `&sort_by=${sort}`
+  }
+  
+  console.log(url)
+  return url;
 }
