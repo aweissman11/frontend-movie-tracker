cdabd7d01a9513a88355e5ef9135ac143a39f1b8
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-29 14:04:31 -0600

    isLoadingReducer

---
+++ src/reducers/isLoadingReducer.js
@@ -0,0 +1,10 @@
+const isLoadingReducer = (state = false, action) => {
+  switch(action.type) {
+    case 'IS_LOADING' :
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+
+export default isLoadingReducer
