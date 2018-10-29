359a05d7fc62ff06665befb4e3aec385abc72e34
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-29 12:39:29 -0600

    combineReducers with searchQuery and filters

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,15 @@ import userReducer from './userReducer';
 import showSingupReducer from './showSignupReducer';
 import showLoginReducer from './showLoginReducer';
 import favoritesReducer from './favoritesReducer';
+import filtersReducer from './filtersReducer';
+import searchQueryReducer from './searchQueryReducer';
 
 export const rootReducer = combineReducers({
   movies: moviesReducer, 
   user: userReducer,
   showLogin: showLoginReducer,
   showSignup: showSingupReducer,
-  favorites: favoritesReducer
+  favorites: favoritesReducer,
+  filters: filtersReducer,
+  searchQuery: searchQueryReducer
 });
