0c8acc254e9a962a4254b4dd76461f1267fb2b29
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-30 07:52:32 -0600

    redundant action imports

--- src/containers/SignUpForm/index.js
+++ src/containers/SignUpForm/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getUserLoggedIn } from '../../actions'
-import { displayLogin } from '../../actions';
 import PropTypes from 'prop-types';
 
 import { getUserLoggedIn, displayLogin, isLoading } from '../../actions'
