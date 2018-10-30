2c5377c34b9d0d9d2d7234b28cef26a9ecff74ca
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-30 12:39:24 -0600

    test deploymodal reducer

--- src/reducers/deployFilterModalReducer.js
+++ src/reducers/deployFilterModalReducer.js
@@ -1,4 +1,4 @@
-const deployFilterModalReducer = (state = false, action) => {
+export const deployFilterModalReducer = (state = false, action) => {
   switch(action.type) {
     case 'DEPLOY_FILTER_MODAL':
       return action.status
