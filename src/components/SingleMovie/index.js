8743aeac4a2873f1618545a73a269898a7aa58d1
Aaron Weissman <weissman.aaron@gmail.com>
2019-12-24 06:46:08 -0700

    correct fetch url and image url

--- src/components/SingleMovie/index.js
+++ src/components/SingleMovie/index.js
@@ -7,27 +7,28 @@ const SingleMovie = (props) => {
   const date = props.release_date;
   const year = date.split('').splice(0, 4).join('');
   return (
-  <div className='single-movie-wrapper' aria-label='single-movie-card'>
-    <div className='favorite-btn-wrapper'>
-      <FavoriteBtn movieId={props.id}/>
+    <div className='single-movie-wrapper' aria-label='single-movie-card'>
+      <div className='favorite-btn-wrapper'>
+        <FavoriteBtn movieId={props.id} />
+      </div>
+      {
+        props.poster_path ?
+          <img
+            src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${props.poster_path}`}
+            alt={props.title}
+          /> :
+          <img
+            src={'http://www.beguilingbooksandart.com/wp-content/uploads/2015/01/at_marcy.png'}
+            alt={props.title}
+          />
+      }
+      <section className='movie-poster-text' aria-label='movie-info'>
+        <h2>{props.title}</h2>
+        <h3>{year}</h3>
+      </section>
     </div>
-    {
-      props.poster_path ?
-    <img
-      src={`https://image.tmdb.org/t/p/w400_and_h600_bestv2${props.poster_path}`} 
-      alt={props.title}
-      /> :
-    <img
-      src={'http://www.beguilingbooksandart.com/wp-content/uploads/2015/01/at_marcy.png'} 
-      alt={props.title}
-      />
-    }
-    <section className='movie-poster-text' aria-label='movie-info'>
-      <h2>{props.title}</h2>
-      <h3>{year}</h3>
-    </section>
-  </div>
-)};
+  )
+};
 
 
 SingleMovie.propTypes = {
